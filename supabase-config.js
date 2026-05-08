13f8109a5c3112afd72f48d4daf9597bb1dbdc67
dhaddaandco <somya.dco@gmail.com>
2026-05-08 14:42:57 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.DCO_SUPABASE_CONFIG = {
-  url: "sb_publishable_xEx8owWZg6mLaA85qnOnRQ_WlPBwsmQ",
+  url: "https://jmtsmowgyicmjlbrvccr.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptdHNtb3dneWljbWpsYnJ2Y2NyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyMjQ1NDksImV4cCI6MjA5MzgwMDU0OX0.IDttOBElQUggAtOSnL7C2gmSVIucEQBOhqALFSZ8KUg"
 };
