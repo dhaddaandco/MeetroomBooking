1001d31491d1a84d31b37e57e3009a631cbbe5e8
dhaddaandco <somya.dco@gmail.com>
2026-05-08 14:35:50 +0530

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.DCO_SUPABASE_CONFIG = {
+  url: "sb_publishable_xEx8owWZg6mLaA85qnOnRQ_WlPBwsmQ",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptdHNtb3dneWljbWpsYnJ2Y2NyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyMjQ1NDksImV4cCI6MjA5MzgwMDU0OX0.IDttOBElQUggAtOSnL7C2gmSVIucEQBOhqALFSZ8KUg"
+};
